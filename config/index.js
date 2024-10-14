12d4c883690f3157c7ff473db9c2517bacb408f4
songmok <118712217+songmok@users.noreply.github.com>
2024-10-15 04:00:00 +0900

    no message

--- config/index.js
+++ config/index.js
@@ -1,5 +1,4 @@
 export const DATABASE_ID = process.env.NOTION_DATABASE_ID;
 export const DATABASE_ID2 = process.env.NOTION_DATABASE_ID2;
 export const TOKEN = process.env.NOTION_TOKEN;
-export const TtokenAndJson = `?access_token=${process.env.TISTORY_TK}&output=json`;
 export const PageNumber = "&page=1";
